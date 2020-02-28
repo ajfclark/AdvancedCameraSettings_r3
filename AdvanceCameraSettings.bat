07a28f9bcad34bdd73f12b573b691662db45a5f7
Andrew Clark <ajfclark@gmail.com>
2020-02-28 17:06:50 +1100

    Added batch files to compile, create a camera config and load a config.

---
+++ AdvanceCameraSettings.bat
@@ -0,0 +1,2 @@
+"\Program Files (x86)\Java\jdk1.8.0_191\bin\java.exe" -cp src;lib\dsj.jar AdvancedCameraSettingsMain
+pause
---
+++ compile.bat
@@ -0,0 +1,1 @@
+"\Program Files (x86)\Java\jdk1.8.0_191\bin\javac.exe" -cp lib\dsj.jar -sourcepath src src\AdvancedCameraSettingsMain.java
